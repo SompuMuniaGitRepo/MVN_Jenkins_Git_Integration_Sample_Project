ee5196331845e06559344a045e08fd260bfe281e
SompuMuniaGitRepo <39269440+SompuMuniaGitRepo@users.noreply.github.com>
2018-07-22 21:54:16 +0530

    Committing changes made to a single class

--- src/test/java/com/dd/pagetests/HomePageTest.java
+++ src/test/java/com/dd/pagetests/HomePageTest.java
@@ -1,11 +1,28 @@
 package com.dd.pagetests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePageTest {
 
-	@Test
+	@Test(priority=1)
 	public void homepageTest() {
 		  System.out.println("Testing Home page");
+	}
+	
+	@Test(priority= 2)
+	public void testfail()  {
+		Assert.fail("Deliberately failing this TC");
+	}
+	
+	@Test(priority=3)
+	public void testpass() {
+		Assert.assertTrue(1>0);
+		
+	}
+	
+	@Test(priority=4,dependsOnMethods= {"testfail"})
+	public void testskip() {
+		System.out.println("This method should be skipped");
 	}
 }
